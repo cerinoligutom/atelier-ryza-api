71ebc0bc064f0311de179c9352869826fa9671e3
Cerino O. Ligutom III <zeferinix@gmail.com>
2019-11-09 01:07:12 +0800

    Preprocessed FieldMixMapInfo

--- src/data/FieldMixMapInfo/index.ts
+++ src/data/FieldMixMapInfo/index.ts
@@ -1,8 +1,48 @@
 import { preprocessData } from '../_common/preprocessData';
 import { getProcessedData } from '../_common/getProcessedData';
 
+interface IFieldMixMapInfoXML {
+  Root: {
+    FieldMixMapInfo: Array<{
+      $: {
+        No: string;
+        stringID: string;
+        mapPattern: string;
+        mapGroup: string;
+        element: string;
+        collect: string;
+        gimmickModel: string;
+        enemy: string;
+        boss: string;
+        point: string;
+        sky: string;
+      };
+    }>;
+  };
+}
+
+interface IFieldMixMapInfo {
+  no: string;
+  stringId: string;
+  enemyId: string;
+  bossId: string;
+}
+
 const FILE_NAME = 'FieldMixMapInfo';
 
-preprocessData(FILE_NAME, __dirname, data => data);
+preprocessData<IFieldMixMapInfoXML, IFieldMixMapInfo[]>(FILE_NAME, __dirname, data =>
+  data.Root.FieldMixMapInfo.map(x => {
+    const { No, stringID, enemy, boss } = x.$;
+
+    const processedData: IFieldMixMapInfo = {
+      bossId: boss,
+      enemyId: enemy,
+      no: No,
+      stringId: stringID,
+    };
+
+    return processedData;
+  }),
+);
 
 export const fieldMixMapInfo = getProcessedData(FILE_NAME, __dirname);
