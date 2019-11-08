42af5e8404c1d55aeaca2d1dad66179deed4db20
Cerino O. Ligutom III <zeferinix@gmail.com>
2019-11-09 01:11:28 +0800

    Preprocessed StrMonsterName

--- src/data/str_monster_name/index.ts
+++ src/data/str_monster_name/index.ts
@@ -1,8 +1,35 @@
 import { preprocessData } from '../_common/preprocessData';
 import { getProcessedData } from '../_common/getProcessedData';
 
+interface IStrMonsterNameXML {
+  Root: {
+    str: Array<{
+      $: {
+        String_No: string;
+        Text: string;
+      };
+    }>;
+  };
+}
+
+interface IStrMonsterName {
+  no: string;
+  text: string;
+}
+
 const FILE_NAME = 'str_monster_name';
 
-preprocessData(FILE_NAME, __dirname, data => data);
+preprocessData<IStrMonsterNameXML, IStrMonsterName[]>(FILE_NAME, __dirname, data =>
+  data.Root.str.map(x => {
+    const { String_No, Text } = x.$;
+
+    const processedData: IStrMonsterName = {
+      no: String_No,
+      text: Text,
+    };
+
+    return processedData;
+  }),
+);
 
 export const strMonsterName = getProcessedData(FILE_NAME, __dirname);
