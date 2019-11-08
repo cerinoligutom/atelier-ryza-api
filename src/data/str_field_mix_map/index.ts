057d0f3515466016a757ed73ae82188412b48e3d
Cerino O. Ligutom III <zeferinix@gmail.com>
2019-11-09 01:10:02 +0800

    Preprocessed StrFieldMixMap

--- src/data/str_field_mix_map/index.ts
+++ src/data/str_field_mix_map/index.ts
@@ -1,8 +1,35 @@
 import { preprocessData } from '../_common/preprocessData';
 import { getProcessedData } from '../_common/getProcessedData';
 
+interface IStrFieldMixMapXML {
+  Root: {
+    str: Array<{
+      $: {
+        String_No: string;
+        Text: string;
+      };
+    }>;
+  };
+}
+
+interface IStrFieldMixMap {
+  no: string;
+  text: string;
+}
+
 const FILE_NAME = 'str_field_mix_map';
 
-preprocessData(FILE_NAME, __dirname, data => data);
+preprocessData<IStrFieldMixMapXML, IStrFieldMixMap[]>(FILE_NAME, __dirname, data =>
+  data.Root.str.map(x => {
+    const { String_No, Text } = x.$;
+
+    const processedData: IStrFieldMixMap = {
+      no: String_No,
+      text: Text,
+    };
+
+    return processedData;
+  }),
+);
 
 export const strFieldMixMap = getProcessedData(FILE_NAME, __dirname);
