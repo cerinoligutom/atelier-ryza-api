addec4ca125d6cfb41afbac4cc6cd74e1058e2f6
Cerino O. Ligutom III <zeferinix@gmail.com>
2019-11-11 02:14:26 +0800

    Ordered search by level then cost

--- src/core/services/item-drops/item-drops.service.ts
+++ src/core/services/item-drops/item-drops.service.ts
@@ -4,24 +4,19 @@ import * as _ from 'lodash';
 import { orderBy } from 'natural-orderby';
 
 function findByName(name: string, levelLimit: number = 100) {
-  const DEFAULT_ORDER_BY_DIRECTION = OrderByDirection.DESC;
-
   const primaryItemResults = qbFieldMixMap
     .like('primaryItemName', name)
     .lessThan('level', levelLimit + 1)
-    .orderBy('level', DEFAULT_ORDER_BY_DIRECTION)
     .exec();
 
   const secondaryItemResults = qbFieldMixMap
     .like('secondaryItemName', name)
     .lessThan('level', levelLimit + 1)
-    .orderBy('level', DEFAULT_ORDER_BY_DIRECTION)
     .exec();
 
   let results = [...primaryItemResults, ...secondaryItemResults];
   results = _.uniqBy(results, 'password');
-  results = orderBy(results, v => v.level, DEFAULT_ORDER_BY_DIRECTION);
-
+  results = orderBy(results, [v => v.level, v => v.cost], [OrderByDirection.DESC, OrderByDirection.DESC]);
   return results;
 }
 
